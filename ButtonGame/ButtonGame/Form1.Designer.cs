6c3c913e8dc486e34989f4d31b155188712940f1
mwkim0402 <mwkim0402@naver.com>
2019-10-04 14:07:54 +0900

    C# 2개의 같은그림 찾기 게임

---
+++ ButtonGame/ButtonGame/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace ButtonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.시작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(126, 0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(128, 128);
+            this.btn1.TabIndex = 1;
+            this.btn1.Tag = "-1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(252, 0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(128, 128);
+            this.btn2.TabIndex = 2;
+            this.btn2.Tag = "-1";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(378, 0);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(128, 128);
+            this.btn3.TabIndex = 3;
+            this.btn3.Tag = "-1";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(378, 126);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(128, 128);
+            this.btn7.TabIndex = 7;
+            this.btn7.Tag = "-1";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(252, 126);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(128, 128);
+            this.btn6.TabIndex = 6;
+            this.btn6.Tag = "-1";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(126, 126);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(128, 128);
+            this.btn5.TabIndex = 5;
+            this.btn5.Tag = "-1";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(0, 126);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(128, 128);
+            this.btn4.TabIndex = 4;
+            this.btn4.Tag = "-1";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn15
+            // 
+            this.btn15.Location = new System.Drawing.Point(378, 378);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(128, 128);
+            this.btn15.TabIndex = 15;
+            this.btn15.Tag = "-1";
+            this.btn15.UseVisualStyleBackColor = true;
+            this.btn15.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.Location = new System.Drawing.Point(252, 378);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(128, 128);
+            this.btn14.TabIndex = 14;
+            this.btn14.Tag = "-1";
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(126, 377);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(128, 128);
+            this.btn13.TabIndex = 13;
+            this.btn13.Tag = "-1";
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(0, 378);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(128, 128);
+            this.btn12.TabIndex = 12;
+            this.btn12.Tag = "-1";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(378, 252);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(128, 128);
+            this.btn11.TabIndex = 11;
+            this.btn11.Tag = "-1";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(252, 252);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(128, 128);
+            this.btn10.TabIndex = 10;
+            this.btn10.Tag = "-1";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(126, 252);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(128, 128);
+            this.btn9.TabIndex = 9;
+            this.btn9.Tag = "-1";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(0, 252);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(128, 128);
+            this.btn8.TabIndex = 8;
+            this.btn8.Tag = "-1";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(509, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 시작ToolStripMenuItem
+            // 
+            this.시작ToolStripMenuItem.Name = "시작ToolStripMenuItem";
+            this.시작ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.시작ToolStripMenuItem.Text = "시작";
+            this.시작ToolStripMenuItem.Click += new System.EventHandler(this.시작ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(17, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 36);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "점수";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "딸기.png");
+            this.imageList1.Images.SetKeyName(1, "레몬.png");
+            this.imageList1.Images.SetKeyName(2, "모과.png");
+            this.imageList1.Images.SetKeyName(3, "배.png");
+            this.imageList1.Images.SetKeyName(4, "사과.png");
+            this.imageList1.Images.SetKeyName(5, "수박.png");
+            this.imageList1.Images.SetKeyName(6, "파인애플.png");
+            this.imageList1.Images.SetKeyName(7, "포도.png");
+            this.imageList1.Images.SetKeyName(8, "check.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn0);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.btn15);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn14);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn13);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn12);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn11);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn10);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Location = new System.Drawing.Point(0, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 508);
+            this.panel1.TabIndex = 18;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(0, 0);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(128, 128);
+            this.btn0.TabIndex = 0;
+            this.btn0.Tag = "-1";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(114, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 24);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "0";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(208, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 34);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "진행 시간";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbTime.ForeColor = System.Drawing.Color.Red;
+            this.lbTime.Location = new System.Drawing.Point(357, 39);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(27, 28);
+            this.lbTime.TabIndex = 21;
+            this.lbTime.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 590);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTime;
+    }
+}
+
---
+++ ButtonGame/ButtonGame/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonGame
+{
+    public partial class Form1 : Form
+    {
+        int firstindex = -1;
+        int secondindex = -1;
+        int FirstTag = -1;
+        int secondTag = -1;
+        int point = 0;
+        int timeSeconds = 0;
+        int cnt = 0;
+
+        Button[] button;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            button = new Button[] { btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15 };
+            foreach (Button btn in button)
+            {
+                btn.Image = imageList1.Images[8];
+                btn.Enabled = false;
+            }
+        }
+
+        private void 시작ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer3.Stop();
+            timeSeconds = 0;
+            lbTime.Text = timeSeconds.ToString();
+            MakeRandomButton();
+            foreach (Button btn in panel1.Controls)
+            {
+                btn.Image = imageList1.Images[Convert.ToInt32(btn.Tag)];
+                btn.Enabled = false;
+            }
+            timer1.Start();
+            cnt = 16;
+        }
+
+        bool FirstClick = true;
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (FirstClick)
+            {
+                firstindex = btn.TabIndex;
+                FirstClick = false;
+                FirstTag = Convert.ToInt32(btn.Tag);
+                btn.Image = imageList1.Images[Convert.ToInt32(btn.Tag)];
+            }
+            else
+            {
+                if (firstindex == btn.TabIndex)
+                {
+                    return;
+                }
+                secondindex = btn.TabIndex;
+                secondTag = Convert.ToInt32(btn.Tag);
+                btn.Image = imageList1.Images[Convert.ToInt32(btn.Tag)];
+
+                timer2.Start();
+                FirstClick = true;
+            }
+        }
+        private void MakeRandomButton()
+        {
+            Random ran = new Random();
+            bool isUnique;
+            int[] ranNum = new int[16];
+            ranNum[0] = ran.Next(0, 16);
+            int cnt = 1;
+            while (cnt < 16)
+            {
+                int temp = ran.Next(0, 16);
+                isUnique = true;
+                for (int i = 0; i < cnt; i++)
+                {
+                    if (ranNum[i] == temp)
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    ranNum[cnt] = temp;
+                    cnt++;
+                }
+            }
+            int index = 0;
+            foreach (Button btn in button)
+            {
+                btn.Tag = ranNum[index] % 8;
+                index++;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            foreach (Button btn in panel1.Controls)
+            {
+                btn.Image = imageList1.Images[8];
+                btn.Enabled = true ;
+            }
+            timer3.Start();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (FirstTag == secondTag)
+            {
+                point += 500;
+                label2.Text = point.ToString();
+                cnt -= 2;
+                button[firstindex].Enabled = false;
+                button[secondindex].Enabled = false;
+                if (cnt == 0)
+                {
+                    timer3.Stop();
+                }
+                timer2.Stop();
+                return;
+            }
+            else
+            button[firstindex].Image = imageList1.Images[8];
+            button[secondindex].Image = imageList1.Images[8];
+            firstindex = -1;
+            secondindex = -1;
+            FirstTag = -1;
+            secondTag = -1;
+            timer2.Stop();
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            timeSeconds++;
+            lbTime.Text = timeSeconds.ToString();
+        }
+    }
+}
