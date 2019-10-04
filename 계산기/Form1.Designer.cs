62361e1cd5f7b40cea2e88118e81a39ca54cfd04
mwkim0402 <mwkim0402@naver.com>
2019-10-04 13:41:58 +0900

    Create 계산기

---
+++ 계산기/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace WindowsFormsApp_0904
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.txtCal = new System.Windows.Forms.TextBox();
+            this.button19 = new System.Windows.Forms.Button();
+            this.backSpace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 76);
+            this.button3.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 131);
+            this.button4.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(86, 131);
+            this.button5.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 131);
+            this.button6.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 39);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 186);
+            this.button7.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 39);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(86, 186);
+            this.button8.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 39);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(160, 186);
+            this.button9.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(56, 39);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button10
+            // 
+            this.button10.ForeColor = System.Drawing.Color.Black;
+            this.button10.Location = new System.Drawing.Point(12, 241);
+            this.button10.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(56, 39);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button11.Location = new System.Drawing.Point(304, 76);
+            this.button11.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(33, 204);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "CE";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // button13
+            // 
+            this.button13.ForeColor = System.Drawing.Color.Blue;
+            this.button13.Location = new System.Drawing.Point(228, 76);
+            this.button13.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(56, 39);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "+";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // button14
+            // 
+            this.button14.ForeColor = System.Drawing.Color.Blue;
+            this.button14.Location = new System.Drawing.Point(228, 131);
+            this.button14.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(56, 39);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "-";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // button15
+            // 
+            this.button15.ForeColor = System.Drawing.Color.Blue;
+            this.button15.Location = new System.Drawing.Point(228, 186);
+            this.button15.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(56, 39);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "x";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // button16
+            // 
+            this.button16.ForeColor = System.Drawing.Color.Blue;
+            this.button16.Location = new System.Drawing.Point(228, 241);
+            this.button16.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(56, 39);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "/";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // txtCal
+            // 
+            this.txtCal.Location = new System.Drawing.Point(12, 21);
+            this.txtCal.Name = "txtCal";
+            this.txtCal.Size = new System.Drawing.Size(377, 44);
+            this.txtCal.TabIndex = 17;
+            this.txtCal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button19
+            // 
+            this.button19.ForeColor = System.Drawing.Color.Red;
+            this.button19.Location = new System.Drawing.Point(86, 241);
+            this.button19.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(130, 39);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "=";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // backSpace
+            // 
+            this.backSpace.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.backSpace.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.backSpace.Location = new System.Drawing.Point(355, 76);
+            this.backSpace.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.backSpace.Name = "backSpace";
+            this.backSpace.Size = new System.Drawing.Size(34, 204);
+            this.backSpace.TabIndex = 20;
+            this.backSpace.Text = "<";
+            this.backSpace.UseVisualStyleBackColor = true;
+            this.backSpace.Click += new System.EventHandler(this.Erase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 296);
+            this.Controls.Add(this.backSpace);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.txtCal);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.Name = "Form1";
+            this.Text = "계산기";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.TextBox txtCal;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button backSpace;
+    }
+}
+
---
+++ 계산기/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_0904
+{
+    public partial class Form1 : Form
+    {
+        bool checkedText = true;  //입력하는 숫자의 처음인지 아닌지 체크
+        int result = 0;
+        int firstNum = 0;  //입력받는 순간 전의 결과값
+        int lastNum = 0;  //입력받는 숫자
+        string beforeOpr = "+"; // 직전에 입력한 연산자
+        string lastOpr = string.Empty; // 현재 입력한 연산자
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // 숫자 or 지우기 버튼 이벤트 처리
+        private void Button_Click(object sender, EventArgs e) //sender => 이벤트를 발생한 객체 자신
+        {
+            // 처음 시작할때, 연산자 입력 후 숫자 입력시 
+            // 첫글자는 클리어 후, 입력
+            // 그 뒤 문자들은 연결 하여 추가로 입력
+            Button btn = (Button)sender;
+            if (checkedText)
+            {
+                this.txtCal.Text += btn.Text; // 복합대입연산자 += 를 사용
+            }
+            else
+            {
+                txtCal.Clear();
+                this.txtCal.Text += btn.Text;
+                checkedText = true;
+            }
+        }
+        // 연산자 버튼 이벤트 처리
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            //시작 
+            Button btn = (Button)sender;
+            lastNum = int.Parse(txtCal.Text);
+            lastOpr = btn.Text;  //입력받은 연산자
+            switch (beforeOpr)
+            {
+                case "+":
+                    result = firstNum + lastNum;
+                    break;
+                case "-":
+                    result = firstNum - lastNum;
+                    break;
+                case "/":
+                    result = firstNum / lastNum;
+                    break;
+                case "x":
+                    result = firstNum * lastNum;
+                    break;
+            }
+            firstNum = result;
+            this.txtCal.Text = result.ToString();
+            beforeOpr = lastOpr;
+            checkedText = false;  // 연산자 입력 후 다음 숫자 입력시 클리어 하기위한 bool
+        }
+        //계산기 리셋 버튼 이벤트(초기 설정)
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            result = 0;
+            firstNum = 0;
+            lastNum = 0;
+            beforeOpr = "+"; // 직전에 입력한 연산자
+            lastOpr = string.Empty; // 현재 입력한 연산자
+            txtCal.Clear();
+        }
+        // 지우기 버튼 이벤트
+        private void Erase_Click(object sender, EventArgs e)
+        {
+            this.txtCal.Text = this.txtCal.Text.Substring(0, txtCal.Text.Length - 1);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keys temp = e.KeyCode;
+        }
+    }
+}
