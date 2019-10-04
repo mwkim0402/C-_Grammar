191582a614b5708d57cb984b8c556615aab40fdb
mwkim0402 <mwkim0402@naver.com>
2019-10-04 17:42:54 +0900

    간단한 생산관리 프로그램

---
+++ Production_Management/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.nuQty = new System.Windows.Forms.NumericUpDown();
+            this.cboProduct = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboWorker = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.dtWork = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboMachine = new System.Windows.Forms.ComboBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nuQty)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "작업자";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(30, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 220);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.ListBox1_DoubleClick);
+            // 
+            // nuQty
+            // 
+            this.nuQty.Location = new System.Drawing.Point(343, 214);
+            this.nuQty.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nuQty.Name = "nuQty";
+            this.nuQty.Size = new System.Drawing.Size(83, 21);
+            this.nuQty.TabIndex = 3;
+            // 
+            // cboProduct
+            // 
+            this.cboProduct.FormattingEnabled = true;
+            this.cboProduct.Location = new System.Drawing.Point(343, 178);
+            this.cboProduct.Name = "cboProduct";
+            this.cboProduct.Size = new System.Drawing.Size(121, 20);
+            this.cboProduct.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(300, 271);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "제품명";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "작업수량";
+            // 
+            // cboWorker
+            // 
+            this.cboWorker.FormattingEnabled = true;
+            this.cboWorker.Location = new System.Drawing.Point(343, 106);
+            this.cboWorker.Name = "cboWorker";
+            this.cboWorker.Size = new System.Drawing.Size(121, 20);
+            this.cboWorker.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnWrite);
+            this.groupBox1.Controls.Add(this.dtWork);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(474, 60);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "작업일자";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(377, 22);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 9;
+            this.btnWrite.Text = "파일저장";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.BtnWrite_Click);
+            // 
+            // dtWork
+            // 
+            this.dtWork.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtWork.Location = new System.Drawing.Point(18, 24);
+            this.dtWork.Name = "dtWork";
+            this.dtWork.Size = new System.Drawing.Size(98, 21);
+            this.dtWork.TabIndex = 8;
+            this.dtWork.ValueChanged += new System.EventHandler(this.DtWork_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "작업기계";
+            // 
+            // cboMachine
+            // 
+            this.cboMachine.FormattingEnabled = true;
+            this.cboMachine.Location = new System.Drawing.Point(343, 142);
+            this.cboMachine.Name = "cboMachine";
+            this.cboMachine.Size = new System.Drawing.Size(121, 20);
+            this.cboMachine.TabIndex = 12;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(389, 271);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 319);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboMachine);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboWorker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cboProduct);
+            this.Controls.Add(this.nuQty);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "작업일지";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nuQty)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown nuQty;
+        private System.Windows.Forms.ComboBox cboProduct;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboWorker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtWork;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboMachine;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Production_Management/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        struct DailyWork
+        {
+            public string WorkDate;
+            public string Worker;
+            public string WorkMachine;
+            public string WorkProduct;
+            public int WorkQty;
+
+            public DailyWork(string date, string worker, string machine, string product, int qty)
+            {
+                WorkDate = date;
+                Worker = worker;
+                WorkMachine = machine;
+                WorkProduct = product;
+                WorkQty = qty;
+            }
+        }
+
+        List<DailyWork> workList = new List<DailyWork>();
+        bool editMode = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initComboBinding();
+            DtWork_ValueChanged(null, null);
+        }
+
+        // 코드내 사용할 공통 바인딩 함수
+        private void initComboBinding()
+        {
+            var cboDic = new Dictionary<string, string>();
+            cboDic.Add("201001", "홍길동");
+            cboDic.Add("201002", "임꺽정");
+            cboDic.Add("201003", "유재석");
+            cboDic.Add("201004", "김구라");
+
+            cboWorker.DataSource = new BindingSource(cboDic, null);
+            cboWorker.DisplayMember = "Value";
+            cboWorker.ValueMember = "Key";
+            cboWorker.SelectedIndex = -1;
+
+            cboDic = new Dictionary<string, string>();
+            cboDic.Add("A1", "A1");
+            cboDic.Add("B1", "B1");
+            cboDic.Add("A3", "A3");
+            cboDic.Add("A4", "A4");
+
+            cboMachine.DataSource = new BindingSource(cboDic, null);
+            cboMachine.DisplayMember = "Value";
+            cboMachine.ValueMember = "Key";
+            cboMachine.SelectedIndex = -1;
+
+            cboDic = new Dictionary<string, string>();
+            cboDic.Add("A001", "꼬깔콘");
+            cboDic.Add("B001", "치토스");
+            cboDic.Add("A003", "포카칩");
+            cboDic.Add("A004", "칙촉");
+
+            cboProduct.DataSource = new BindingSource(cboDic, null);
+            cboProduct.DisplayMember = "Value";
+            cboProduct.ValueMember = "Key";
+            cboProduct.SelectedIndex = -1;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            string date = dtWork.Value.ToShortDateString();
+            string worker = cboWorker.SelectedValue.ToString();
+            string machine = cboMachine.SelectedValue.ToString();
+            string product = cboProduct.SelectedValue.ToString();
+            int qty = (int)nuQty.Value;
+
+            if (editMode)
+            {
+                //데이터 수정
+                int idx = listBox1.SelectedIndex;
+
+                DailyWork workItem = new DailyWork(date, worker, machine, product, qty);
+                workList[idx] = workItem;
+                listBox1.Items[idx] = string.Format("{0}|{1}|{2}|{3}|{4}", date, worker, machine, product, qty);
+            }
+            else
+            {
+                //신규 저장
+                DailyWork workItem = new DailyWork(date, worker, machine, product, qty);
+                workList.Add(workItem);
+                listBox1.Items.Add(string.Format("{0}|{1}|{2}|{3}|{4}", date, worker, machine, product, qty));
+            }
+        }
+
+        private void DtWork_ValueChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            workList.Clear();
+            ClearValue();
+
+            //해당하는 작업일자의 파일이 있다면 읽어서 List를 채운다.
+            ReadWorkFile();
+        }
+
+        private void ClearValue()
+        {
+            cboWorker.SelectedIndex = -1;
+            cboMachine.SelectedIndex = -1;
+            cboProduct.SelectedIndex = -1;
+            nuQty.Value = 0;
+
+            editMode = false;
+        }
+
+        private void BtnWrite_Click(object sender, EventArgs e)
+        {
+            string cdate = dtWork.Value.ToString("yyyyMMdd");
+            string format = "{0,10}|{1,-10}|{2,-10}|{3,-15}|{4,10}";
+            string line;
+
+            //streamwriter 는 using문이 종료되는 순간 사라진다.
+            using (StreamWriter wr = new StreamWriter(@"C:\Temp\" + cdate + ".txt"))
+            {
+                foreach (DailyWork workItem in workList)
+                {
+                    line = string.Format(format, workItem.WorkDate, workItem.Worker, workItem.WorkMachine, workItem.WorkProduct, workItem.WorkQty);
+                    wr.WriteLine(line);
+                }
+            }
+        }
+
+        private void ReadWorkFile()
+        {
+            string cdate = dtWork.Value.ToString("yyyyMMdd");
+            string line;
+
+            if (!File.Exists(@"C:\Temp\" + cdate + ".txt"))
+                return;
+
+            using (StreamReader rdr = new StreamReader(@"C:\Temp\" + cdate + ".txt"))
+            {
+                while ((line = rdr.ReadLine()) != null)
+                {
+                    string[] workArr = line.Split('|');
+                    if (workArr.Length == 5)
+                    {
+                        DailyWork workItem = new DailyWork(workArr[0].Trim(), workArr[1].Trim(), workArr[2].Trim(), workArr[3].Trim(), int.Parse(workArr[4].Trim()));
+                        workList.Add(workItem);
+                    }
+                }
+            }
+
+            foreach (DailyWork workItem in workList)
+            {
+                listBox1.Items.Add(string.Format("{0}|{1}|{2}|{3}|{4}", workItem.WorkDate, workItem.Worker, workItem.WorkMachine, workItem.WorkProduct, workItem.WorkQty));
+            }
+        }
+
+        private void ListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            DailyWork workItem = workList[listBox1.SelectedIndex];
+
+            cboWorker.SelectedValue = workItem.Worker;
+            cboMachine.SelectedValue = workItem.WorkMachine;
+            cboProduct.SelectedValue = workItem.WorkProduct;
+            nuQty.Value = workItem.WorkQty;
+
+            editMode = true;
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            ClearValue();
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.SuspendLayout();
+        //    // 
+        //    // Form1
+        //    // 
+        //    this.ClientSize = new System.Drawing.Size(208, 119);
+        //    this.Name = "Form1";
+        //    this.ResumeLayout(false);
+        //}
+    }
+}
